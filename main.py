555bfa370a5cd0fdf5fae15c97b96da0a11ae2c6
Nguyen Thanh Vinh <vinhntdata0906@gmail.com>
2025-04-23 10:19:37 +0700

    got some struggle with permisson and os

---
+++ main.py
@@ -0,0 +1,15 @@
+import eel
+import subprocess
+import platform
+import os
+
+eel.init('www')
+url = 'http://localhost:8000/index.html'
+
+if platform.system() == 'Darwin':  # macOS
+    subprocess.Popen(['open', '-a', 'Google Chrome', '--args', f'--app={url}'])
+else:
+    # Windows/Linux fallback
+    os.system('start Chrome.exe --app"http://localhost:8000/index.html"')
+
+eel.start('index.html', mode=None, host='localhost', block=True)
---
+++ www/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('.text').textllite({
+        loop: true,
+        sync: true,
+        in:{
+            effect: "bounceln"
+        },
+
+        out:{
+            effect: "bounceOut"
+        }
+    })
+});
